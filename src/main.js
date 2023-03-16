9336b5acdda25cfb04674d9fadee609fabcf58a3
YuriyPonomarev <yuriy.ponomarev224@gmail.com>
2023-03-16 19:12:55 +0600

    deploy

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/normalize.css'
-import './assets/scss/general.scss'
-import './assets/scss/general_media.scss'
-
-createApp(App).use(router).mount('#app')
